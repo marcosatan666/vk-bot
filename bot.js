1bdebda10753f0fb1dc67ca040ee45c4a91c1e41
marcosatan666 <61208408+marcosatan666@users.noreply.github.com>
2020-07-23 19:49:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,9 @@
 const VkBot = require('node-vk-bot-api');
-//const Markup = require('node-vk-bot-api/lib/markup')
+const Markup = require('node-vk-bot-api/lib/markup')
 const bot = new VkBot(process.env.TOKEN);
 
-      //  bot.command('Начать', (ctx) => {
+ bot.command('Начать', (ctx) => {
 
-        //    ctx.reply('Выберите один из указанных пунктов', null, Markup
-        //  .keyboard([
-        //    [Markup.button('Хочу сделать заказ', 'primary'), Markup.button('Примеры работ', 'positive'), Markup.button('Инстаграм', 'negative')]
-       //   ])
-       //   .oneTime(),
-       // );
-       // })
 const app = require('firebase/app'), firebase = app.initializeApp({
     apiKey: "AIzaSyDLkYdTTDYD9Jlqk96tthmvdZ9Bg9uAyag",
     authDomain: "tg-firebase-react-app.firebaseapp.com",
@@ -22,6 +15,15 @@ const app = require('firebase/app'), firebase = app.initializeApp({
     measurementId: "G-G8ZW8G1XE6"
 });
 require ('firebase/database')
+       
+       
+ctx.reply('Выберите один из указанных пунктов', null, Markup
+   .keyboard([
+     [Markup.button('Хочу сделать заказ', 'primary'), Markup.button('Примеры работ', 'positive'), Markup.button('Инстаграм', 'negative')]
+     ])
+   .oneTime(),
+ );
+ })
 
 bot.command(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, ctx => {
     let text = ctx.message.text
