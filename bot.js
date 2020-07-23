63f89a42271abaf32ceb570255622edc7d7375a2
marcosatan666 <61208408+marcosatan666@users.noreply.github.com>
2020-07-23 14:50:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,8 @@ const VkBot = require('node-vk-bot-api');
  
 const bot = new VkBot(process.env.TOKEN);
  
-bot.command(/^какой сейчас урок(\.|\?)?$/i, (ctx) => {
- let date = new Date(new Date().getTime() + 10800000);
- ctx.reply(date.toString());
+bot.command('/start', (ctx) => {
+  ctx.reply('Hello!');
 });
  
 bot.startPolling();
