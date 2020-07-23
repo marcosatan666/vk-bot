bccb5fe3fd1d317891e72eaf42b6a005744f5ffc
marcosatan666 <61208408+marcosatan666@users.noreply.github.com>
2020-07-23 15:42:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,45 @@
 const VkBot = require('node-vk-bot-api');
- 
+
 const bot = new VkBot(process.env.TOKEN);
- 
-bot.command('/start', (ctx) => {
-  ctx.reply('Hello!');
-});
- 
+
+bot.command(/^\d{1,} (\+|\-|\*|\/|\%|\^) \d{1,}$/, (ctx) => {
+
+    let arr = ctx.message.text.split(" ")
+    
+    try {
+        switch (arr[1]) {
+            case "+": {
+                ctx.reply(parseFloat(arr[0]) + parseFloat(arr[2]))
+                break
+            }
+            case "-": {
+                ctx.reply(parseFloat(arr[0]) - parseFloat(arr[2]))
+                break
+            }
+            case "*": {
+                ctx.reply(parseFloat(arr[0]) * parseFloat(arr[2]))
+                break
+            }
+            case "/": {
+                ctx.reply(parseFloat(arr[0]) / parseFloat(arr[2]))
+                break
+            }
+            case "^": {
+                ctx.reply(parseFloat(arr[0]) ** parseFloat(arr[2]))
+                break
+            }
+            case "%": {
+                ctx.reply(parseFloat(arr[0]) % parseFloat(arr[2]))
+                break
+            }
+        }
+        
+    } catch (e) {
+        console.log(e)
+    }
+
+})
+
+console.log('Бот запущен')
+
 bot.startPolling();
