19d25da5008c25db60a5c8fa9c4308855f3b8590
marcosatan666 <61208408+marcosatan666@users.noreply.github.com>
2020-07-23 18:44:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
-const VkBot = require('node-vk-bot-api')
+const VkBot = require('node-vk-bot-api');
 const Markup = require('node-vk-bot-api/lib/markup')
-const firebase = require('firebase.js')
 
 const bot = new VkBot(process.env.TOKEN);
 
@@ -8,14 +7,23 @@ bot.command('Начать', (ctx) => {
 
     ctx.reply('Выберите один из указанных пунктов', null, Markup
   .keyboard([
-    [Markup.button('Хочу сделать заказ', 'primary'), 
-     Markup.button('Примеры работ', 'positive'), 
-     Markup.button('Инстаграм', 'negative')]
+    [Markup.button('Хочу сделать заказ', 'primary'), Markup.button('Примеры работ', 'positive'), Markup.button('Инстаграм', 'negative')]
   ])
   .oneTime(),
 );
-    
-
 })
 
-bot.startPolling();
+bot.command('Хочу сделать заказ', (ctx) => {
+    //photos.getMessagesUploadServer()
+    ctx.reply('Вам предоставляется прайслист ', null, Markup
+    .keyboard([
+        [Markup.button('В полный рост', 'positive'),
+        Markup.button('По пояс', 'positive')
+        Markup.button('Бюст', 'positive')]
+        ])
+        .oneTime(),
+    );
+    })
+
+
+bot.startPolling()
