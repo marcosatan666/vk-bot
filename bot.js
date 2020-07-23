25f6ba1961be91aafa55a61682fabbf758513c44
marcosatan666 <61208408+marcosatan666@users.noreply.github.com>
2020-07-23 17:29:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,18 @@
 const VkBot = require('node-vk-bot-api');
+const Markup = require('node-vk-bot-api/lib/markup')
 
 const bot = new VkBot(process.env.TOKEN);
 
 bot.command('Начать', (ctx) => {
 
-    ctx.reply('Работать блеять')
+    ctx.reply('Выберите один из указанных пунктов', null, Markup
+  .keyboard([
+    'Хочу сделать заказ',
+    'Примеры работ',
+    'Инстаграм'
+  ])
+  .oneTime(),
+);
     
 
 })
